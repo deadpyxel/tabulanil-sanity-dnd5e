6d815e63cb049537cb5b17292172fca46d010c57
Robson Cruz <ghost0092@gmail.com>
2024-05-12 17:52:48 -0300

    feat(tabulanil-sanity.js): add update method
    Now it will be possible to updating/setting the actors current sanity
    flag using this method.

--- scripts/tabulanil-sanity.js
+++ scripts/tabulanil-sanity.js
@@ -15,12 +15,6 @@ class TabulanilSanity {
   static FLAGS = {
     SANITY_POOL: "totalSanity",
     CURRENT_SANITY: "currSanity",
-    INSANITY_T1: "sanT1",
-    INSANITY_T2: "sanT2",
-    INSANITY_T3: "sanT3",
-    INSANITY_T4: "sanT4",
-    INSANITY_T5: "sanT5",
-    INSANITY_T6: "sanT6",
   }
   
   /**
@@ -53,7 +47,7 @@ class TabulanilSanityData {
   * Calculates the total sanity points for a specified Actor based on their mental ability scores.
   *
   * @param {string} actorId - The unique identifier for the actor whose sanity total we want to calculate
-  * @returns {Promise} A PRomise that resolves when the actor's Sanity flag is updated.
+  * @returns {Promise} A Promise that resolves when the actor's Sanity flag is updated.
   */
   static calcTotalSanityForActor(actorId) {
     const relatedActor = game.actors.get(actorId);
@@ -66,12 +60,17 @@ class TabulanilSanityData {
 
   /**
   * Updates the sanity score for a specified actor.
-  * TODO: This method is currently a placeholder and should be implemented to handle
   *
   * @param {string} actorId - The unique identifier for the actor whose sanity we want to update
   * @param {number} value - The value to adjust the actor's sanity to.
+  * @returns {Promise} A Promise that resolves when the actor's Current Sanity flag is updated.
   */
-  static updateSanityForActor(actorId, value) {}
+  static updateSanityForActor(actorId, value) {
+    const relatedActor = game.actors.get(actorId);
+    if (relatedActor !== undefined) {
+      return relatedActor.setFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.CURRENT_SANITY, value);
+    }
+  }
 }
 
 /**
