6e6d9baf36a66308ab24fb3cfbb98192da702165
Robson Cruz <ghost0092@gmail.com>
2024-05-12 22:56:34 -0300

    refactor(tabulanil-sanity.js): use Actor reference
    Instead of passing ActorID strings around we can directly refer tot he
    actor and remove some of the need for null checks. We also update the
    function to calculate the insanity tier of a character to work with
    percentages instead.

--- scripts/tabulanil-sanity.js
+++ scripts/tabulanil-sanity.js
@@ -47,71 +47,53 @@ class TabulanilSanityData {
   /**
   * Calculates the total sanity points for a specified Actor based on their mental ability scores.
   *
-  * @param {string} actorId - The unique identifier for the actor whose sanity total we want to calculate
+  * @param {Actor} actor - The actor whose sanity total we want to calculate
   * @returns {Promise} A Promise that resolves when the actor's Sanity flag is updated.
   */
-  static calcTotalSanityForActor(actorId) {
-    const relatedActor = game.actors.get(actorId);
-    if (relatedActor !== undefined) {
-      const actorAbilities = relatedActor.system.abilities
-      const totalSanity = (actorAbilities.cha.value + actorAbilities.int.value + actorAbilities.wis.value) * 2
-      return relatedActor.setFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.SANITY_POOL, totalSanity);
-    }
+  static calcTotalSanityForActor(actor) {
+    const actorAbilities = actor.system.abilities
+    const totalSanity = (actorAbilities.cha.value + actorAbilities.int.value + actorAbilities.wis.value) * 2
+    return actor.setFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.SANITY_POOL, totalSanity);
   }
 
   /**
   * Updates the sanity score for a specified actor.
   *
-  * @param {string} actorId - The unique identifier for the actor whose sanity we want to update
+  * @param {Actor} actor - The actor whose sanity total we want to update
   * @param {number} value - The value to adjust the actor's sanity to.
   * @returns {Promise} A Promise that resolves when the actor's Current Sanity flag is updated.
   */
-  static updateSanityForActor(actorId, value) {
-    const relatedActor = game.actors.get(actorId);
-    if (relatedActor !== undefined) {
-      return relatedActor.setFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.CURRENT_SANITY, value);
-    }
+  static updateSanityForActor(actor, value) {
+    return actor.setFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.CURRENT_SANITY, value);
   }
 
   /**
   * Gets the current Sanity points for the specified actor.
   *
-  * @param {string} actorId - The unique identifier for the actor whose sanity we want to update
+  * @param {Actor} actor - The actor whose current sanity we want to fetch
   * @returns {number|null} The current Sanity Points of the actor if available, or null otherwise
   */
-  static getSanityForActor(actorId) {
-    const relatedActor = game.actors.get(actorId);
-    if (relatedActor !== undefined) {
-      return relatedActor.getFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.CURRENT_SANITY);
-    }
-    return null;
+  static getSanityForActor(actor) {
+    return actor.getFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.CURRENT_SANITY);
   }
 
   /**
   * Gets the Total Sanity score for the specified actor.
   *
-  * @param {string} actorId - The unique identifier for the actor whose sanity we want to update
+  * @param {Actor} actor - The actor whose sanity total we want to fetch
   * @returns {number|null} The current Total Sanity Points of the actor if available, or null otherwise
   */
-  static getTotalSanityForActor(actorId) {
-    const relatedActor = game.actors.get(actorId);
-    if (relatedActor !== undefined) {
-      return relatedActor.getFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.SANITY_POOL);
-    }
-    return null;
+  static getTotalSanityForActor(actor) {
+    return actor.getFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.SANITY_POOL);
   }
   /**
   * Gets the current insanity tier for the specified actor.
   *
-  * @param {string} actorId - The unique identifier for the actor whose sanity we want to update
+  * @param {Actor} actor - The actor whose insanity tier we want to query
   * @returns {number|null} The current insanity tier of the actor if available, or null otherwise
   */
-  static getInsanityTierForActor(actorId) {
-    const relatedActor = game.actors.get(actorId);
-    if (relatedActor !== undefined) {
-      return relatedActor.getFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.INSANITY_TIER);
-    }
-    return null;
+  static getInsanityTierForActor(actor) {
+    return actor.getFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.INSANITY_TIER);
   }
   /**
   * Updates the insanity tier for a specified actor.
@@ -119,15 +101,12 @@ class TabulanilSanityData {
   * @param {string} actorId - The unique identifier for the actor whose insanity tier we want to update
   * @returns {Promise} A Promise that resolves when the actor's Insanity Tier flag is updated.
   */
-  static updateCurrentInsanityTierForActor(actorId) {
-    const relatedActor = game.actors.get(actorId);
-    if (relatedActor !== undefined) {
-      const currSan = relatedActor.getFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.CURRENT_SANITY);
-      const totalSan = relatedActor.getFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.SANITY_POOL);
+  static updateCurrentInsanityTierForActor(actor) {
+      const currSan = actor.getFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.CURRENT_SANITY);
+      const totalSan = actor.getFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.SANITY_POOL);
       const sanityTier = this.calcSanityTier(totalSan, [0.8, 0.6, 0.4, 0.2, 0.1, 0.0], currSan);
-      TabulanilSanity.log(false, `Current Insanity Tier for actor ${relatedActor.name}[${actorId}]: ${sanityTier}`);
-      return relatedActor.setFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.INSANITY_TIER, sanityTier);
-    }
+      TabulanilSanity.log(false, `Current Insanity Tier for actor ${actor.name}[${actor}]: ${sanityTier}`);
+      return actor.setFlag(TabulanilSanity.ID, TabulanilSanity.FLAGS.INSANITY_TIER, sanityTier);
   }
 
   /**
@@ -142,23 +121,17 @@ class TabulanilSanityData {
    * @returns {number} The 1-based index of the highest matching tier or 0 if no tier is valid
    */
   static calcSanityTier(baseVal, coef, currVal) {
-    // IF the current value is same as base value, imply no tier was found
-    if (baseVal === currVal) {
-      return 0;
+    if (baseVal <= 0) {
+      return 0
     }
-    // Define a safe default for current tier and iterate of the coefficients
+    const sanPerc = currVal/baseVal
     let currTier = 0;
     for (let i = 0; i < coef.length; i++) {
-      // Calculate the threshold for this tier
-      const tierVal = Math.floor(baseVal * coef[i])
-      // if the current value is greater than the current tier threhold, break out of the loop
-      if (currVal > tierVal) {
+      if (sanPerc > coef[i]) {
         break;
       }
-      // Update the current matching tier
       currTier = i + 1;
     }
-    // Return the tier found, or 0 if no matching tier was found.
     return currTier;
   }
 }
@@ -182,12 +155,12 @@ Hooks.once("devModeReady", ({ registerPackageDebugFlag }) => {
  * @param {Object} data - The data object associated with the actor.
  */
 Hooks.on("renderActorSheet5eCharacter", (app, [html], data) => {
-  TabulanilSanity.log(false, `Opened actor sheet for ${app.document.name}`);
-  const actorId = app.document.id;
+  const actor = app.document;
+  TabulanilSanity.log(false, `Opened actor sheet for ${actor.name}`);
 
-  const totalSanity = TabulanilSanityData.getTotalSanityForActor(actorId);
-  const currSanity = TabulanilSanityData.getSanityForActor(actorId);
-  const currInsanityTier = TabulanilSanityData.getInsanityTierForActor(actorId);
+  const totalSanity = TabulanilSanityData.getTotalSanityForActor(actor);
+  const currSanity = TabulanilSanityData.getSanityForActor(actor);
+  const currInsanityTier = TabulanilSanityData.getInsanityTierForActor(actor);
   if (totalSanity <= 0) {
     return;
   }
@@ -206,13 +179,13 @@ Hooks.on("renderActorSheet5eCharacter", (app, [html], data) => {
           </div>
           <input type="text" name="tabulanil.sanity.value" data-dtype="Number" placeholder="0" value="0" hidden="">
         </div>
-        <div class="tmp">
+        <div class="tmp sanity-tier">
           <input type="text" name="tabulanil.sanity.tier" data-dtype="Number" placeholder="TMP" value="${currInsanityTier}">
         </div>
       </div>
     </div>`
 
-  const actorSheetLocation = html.querySelector(`#ActorSheet5eCharacter2-Actor-${actorId} > section > form > section.sheet-body > div > div > div.card > div.stats > div:nth-child(4)`);
+  const actorSheetLocation = html.querySelector(`#ActorSheet5eCharacter2-Actor-${actor.id} > section > form > section.sheet-body > div > div > div.card > div.stats > div:nth-child(4)`);
   if (actorSheetLocation) {
     actorSheetLocation.insertAdjacentHTML("afterend", sanityUI);
   }
